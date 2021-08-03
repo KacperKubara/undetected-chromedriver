5c721aa0d4db180c55118ddbd5a1c82f64ae7d91
KacperKubara <kacper.kubara.ai@gmail.com>
2021-08-03 14:14:49 +0200

    fix

--- undetected_chromedriver/v2.py
+++ undetected_chromedriver/v2.py
@@ -179,7 +179,8 @@ class Chrome(selenium.webdriver.chrome.webdriver.WebDriver):
 
         debug_port = selenium.webdriver.common.service.utils.free_port()
         debug_host = "127.0.0.1"
-
+        print(debug_port)
+        
         if not options.debugger_address:
             options.debugger_address = "%s:%d" % (debug_host, debug_port)
 
